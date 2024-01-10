cc849c3a9b61b871443570e2cd073b33a1308ac1
Lowell-37 <a2484822206@126.com>
2024-01-10 22:38:28 +0800

    latest version

---
+++ src/components/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+  <a-list item-layout="horizontal" :data-source="props.postList">
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <a-list-item-meta :description="item.content">
+          <template #title>
+            <a href="https://www.antdv.com/">{{ item.title }}</a>
+          </template>
+          <template #avatar>
+            <a-avatar :src="mountain" />
+          </template>
+        </a-list-item-meta>
+      </a-list-item>
+    </template>
+  </a-list>
+</template>
+
+<script setup lang="ts">
+import mountain from "../assets/mountain.jpg";
+import { defineProps, withDefaults } from "vue";
+
+interface Props {
+  postList: any[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  postList: () => [],
+});
+</script>
+<style scoped>
+.mountain {
+  width: 200px;
+}
+</style>
